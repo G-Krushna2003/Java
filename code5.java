be3e0d4d9a40e17dbcd2c669ed62b1706e83e22c
krushna <krishgomsale@gmail.com>
2024-09-17 22:24:50 +0530

    code5

---
+++ code5.java
@@ -0,0 +1,18 @@
+/*
+ 5. Write a program to check whether the given number is divisible by 7 or not.
+Input : 105
+Output: Divisible by 7
+Input: -31
+Output: Not divisible by 7
+ */
+public class code5 {
+    public static void main(String[] args) {
+        int num=-31;
+        if(num%7==0){
+            System.out.println("Divisible by 7");
+        }else{
+            System.out.println("Not divisible by 7");
+        }
+    }
+    
+}
