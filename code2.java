fef66f46541d290bf9a73e4547ee310cc4365ceb
krushna <krishgomsale@gmail.com>
2024-09-17 21:49:50 +0530

    second code

---
+++ code2.java
@@ -0,0 +1,18 @@
+
+/*2. Write a program to check whether the given number is even or odd
+Input: num =13;
+Output: 13 is an odd number
+Input: num =8;
+Output: 8 is an even number */
+public class code2 {
+    public static void main(String[] args) {
+        int num=8;
+
+        if(num%2==0){
+            System.out.println("Even number ");
+        }else{
+            System.out.println("odd number");
+        }
+    }
+    
+}
