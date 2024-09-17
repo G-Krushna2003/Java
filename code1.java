cbef964d75bff053ce52ad860ea48e03a3e34c91
krushna <krishgomsale@gmail.com>
2024-09-17 21:47:54 +0530

    first code

---
+++ code1.java
@@ -0,0 +1,28 @@
+/*
+ 1. Write a program to check whether the given number is positive or not.(take hardcoded
+values)
+Input: num = 5;
+Output: 5 is a positive number.
+Input: num = -9;
+Output: -9 is a negative number.
+ */
+
+
+
+public class code1 {
+
+    public static void main(String[] args) {
+        
+        int num=-9;
+
+        if(num>=0){
+            System.out.println("Positive Number");
+        }else{
+            System.out.println("Negative Number");
+        }
+    }
+
+
+    
+    
+}
